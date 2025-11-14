c2656ce6d23b16a461d8b6690d6d1621bd286862
Your Name <HellOShael>
2025-11-14 08:46:08 +0100

    chore: initial OpenTofu + MinIO setup

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    minio = {
+      source  = "terraform-provider-minio/minio"
+      version = ">= 3.1.0"
+    }
+  }
+}
+
+provider "minio" {
+  minio_server   = "localhost:9000"   # ou "127.0.0.1:9000"
+  minio_user     = "minioadmin"
+  minio_password = "minioadmin"
+  # minio_region   = "us-east-1"      # optionnel
+  # minio_ssl      = false            # optionnel si tu es en http
+}
+
+resource "minio_s3_bucket" "tp1_bucket" {
+  bucket = "tp1-cloud-bucket"
+  acl    = "private"
+}

1386d01fb22364bfc23eb0d1c6f554fc197a52f9
Your Name <HellOShael>
2025-11-16 23:17:57 +0100

    refactor: utilisation des variables dans le main.tf

--- main.tf
+++ main.tf
@@ -8,27 +8,27 @@ terraform {
 }
 
 provider "minio" {
-  minio_server   = "127.0.0.1:9000"
-  minio_user     = "minioadmin"
-  minio_password = "minioadmin"
+  minio_server   = var.minio_server
+  minio_user     = var.minio_user
+  minio_password = var.minio_password
   minio_ssl      = false
 }
 
 resource "minio_s3_bucket" "web_bucket" {
-  bucket = "webbucket"
+  bucket = var.bucket_name
   acl    = "public-read"
 }
 
 resource "minio_s3_object" "index_html" {
   bucket_name  = minio_s3_bucket.web_bucket.bucket
   object_name  = "index.html"
-  source       = "index.html" # fichier local
+  source       = "index.html"
   content_type = "text/html"
 }
 
 resource "minio_s3_object" "style_css" {
   bucket_name  = minio_s3_bucket.web_bucket.bucket
   object_name  = "style.css"
-  source       = "style.css" # fichier local
+  source       = "style.css"
   content_type = "text/css"
 }

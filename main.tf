a0b34a722f7307ee735b408d82e2ab2384eb1114
Your Name <HellOShael>
2025-11-16 22:26:05 +0100

    feat: deploiement du site sur Minio

--- main.tf
+++ main.tf
@@ -8,13 +8,27 @@ terraform {
 }
 
 provider "minio" {
-  minio_server     = "127.0.0.1:9000"
-  minio_access_key = "minioadmin"
-  minio_secret_key = "minioadmin"
-  minio_ssl        = false
+  minio_server   = "127.0.0.1:9000"
+  minio_user     = "minioadmin"
+  minio_password = "minioadmin"
+  minio_ssl      = false
 }
 
-resource "minio_s3_bucket" "tp1_bucket" {
-  bucket = "tp1-cloud-bucket"
-  acl    = "private"
+resource "minio_s3_bucket" "web_bucket" {
+  bucket = "webbucket"
+  acl    = "public-read"
+}
+
+resource "minio_s3_object" "index_html" {
+  bucket_name  = minio_s3_bucket.web_bucket.bucket
+  object_name  = "index.html"
+  source       = "index.html" # fichier local
+  content_type = "text/html"
+}
+
+resource "minio_s3_object" "style_css" {
+  bucket_name  = minio_s3_bucket.web_bucket.bucket
+  object_name  = "style.css"
+  source       = "style.css" # fichier local
+  content_type = "text/css"
 }
